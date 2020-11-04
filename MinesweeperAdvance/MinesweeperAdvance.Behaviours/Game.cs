debc83c678c4c630b0e0bddc0aec0c1124faf4c8
InsertAReallyCreativeNameHere <60645493+InsertAReallyCreativeNameHere@users.noreply.github.com>
2020-11-05 08:40:03 +1100

    hsttjysjrw56iyjsrrw

---
+++ MinesweeperAdvance/MinesweeperAdvance.Behaviours/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinesweeperAdvance.Behaviours
+{
+    public static class Game
+    {
+        public static async Task<int> Test(int i)
+        {
+            await Task.Yield();
+
+            MessageBox.Show("Wow called through C#.");
+
+            return i;
+        }
+    }
+}
---
+++ MinesweeperAdvance/MinesweeperAdvance/Minesweeper.vb
@@ -0,0 +1,8 @@
+﻿Imports MinesweeperAdvance.Behaviours
+
+Public Class Minesweeper
+    Async Sub Main()
+        MessageBox.Show("Wow it runs.")
+        Await Game.Test(420)
+    End Sub
+End Class
