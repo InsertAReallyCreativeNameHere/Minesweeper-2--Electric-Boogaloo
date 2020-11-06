21524728834af5d0234d51be9c01a93924aa824c
InsertAReallyCreativeNameHere <60645493+InsertAReallyCreativeNameHere@users.noreply.github.com>
2020-11-06 13:22:15 +1100

    Made an attempt to test drawing using .NET wingdi+ functions.
    I fucked it up.

--- MinesweeperAdvance/MinesweeperAdvance.Behaviours/Game.cs
+++ MinesweeperAdvance/MinesweeperAdvance.Behaviours/Game.cs
@@ -4,18 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace MinesweeperAdvance.Behaviours
 {
     public static class Game
     {
+        public static Graphics graphics;
         public static int Test(int i)
         {
-            //await Task.Yield();
-
-            MessageBox.Show("Wow called through C#.");
-
+            using (SolidBrush brush = new SolidBrush(Color.Red))
+            {
+                graphics.FillRectangle(brush, new Rectangle(0, 0, 1000, 1000));
+            }
+            using (Pen is_ = new Pen(Color.Blue, 5))
+            {
+                graphics.DrawLine(is_, 20, 10, 200, 200);
+            }
             return i;
         }
+    }
+
+    public class Tile : IDisposable
+    {
+        public (uint, uint) position;
+
+        #region Dispose
+        private bool disposedValue;
+        protected virtual void Dispose(bool disposing) // I'm a C++ programmer, we do RAII where I come from.
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Undraw stuff here.
+                }
+
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+    public class TileMap : IDisposable
+    {
+        public (uint, uint) size;
+        public Tile[] tiles;
+
+        #region Dispose
+        private bool disposedValue;
+        protected virtual void Dispose(bool disposing) // I'm a C++ programmer, we do RAII here.
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Destroy all tiles here.
+                }
+
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
--- MinesweeperAdvance/MinesweeperAdvance/Minesweeper.vb
+++ MinesweeperAdvance/MinesweeperAdvance/Minesweeper.vb
@@ -1,8 +1,22 @@
 ﻿Imports MinesweeperAdvance.Behaviours
+Imports System.Windows.Forms
 
-Public Class Minesweeper
-    Sub Main()
-        MessageBox.Show("Wow it runs.", "bruh")
+Partial Public Class Minesweeper
+    Inherits Form
+
+    Private Sub Minesweeper_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Game.graphics = Me.CreateGraphics()
         Game.Test(420)
     End Sub
+
 End Class
+
+Namespace MinesweeperAdvance
+    Class Entry
+        Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(True)
+            Application.Run(New Minesweeper())
+        End Sub
+    End Class
+End Namespace
