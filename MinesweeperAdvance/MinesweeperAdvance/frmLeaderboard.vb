91a3302445cccd817d0f4053496f58033e77912f
Andrew EYHORN <andrew.eyhorn@gmail.com>
2020-11-13 09:57:39 +1100

    Worked on leaderboard panel, however might change it to single score

---
+++ MinesweeperAdvance/MinesweeperAdvance/frmLeaderboard.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+Public Class frmLeaderboard
+    Private Sub frmLeaderboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim leaderboardFile As StreamReader = File.OpenText("Leaderboard.txt")
+        Do While leaderboardFile.Peek <> -1
+            lstLeaderboard.Items.Add(leaderboardFile.ReadLine())
+        Loop
+        leaderboardFile.Close()
+    End Sub
+
+    Private Sub btnReturnFromLeaderboard_Click(sender As Object, e As EventArgs) Handles btnReturnFromLeaderboard.Click
+        Dim leaderboardFile2 As StreamWriter = File.CreateText("Leaderboard.txt")
+        For Each Name As Object In lstLeaderboard.Items
+            leaderboardFile2.WriteLine(Name)
+        Next
+        leaderboardFile2.Close()
+        frmTitleScreen.Show()
+        Me.Hide()
+    End Sub
+
+
+End Class
--- MinesweeperAdvance/MinesweeperAdvance/frmTitleScreen.vb
+++ MinesweeperAdvance/MinesweeperAdvance/frmTitleScreen.vb
@@ -1,7 +1,4 @@
-﻿Imports MinesweeperAdvance.Behaviours
-
-
-
+﻿
 Public Class frmTitleScreen
 
     Private Sub frmTitleScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -25,6 +22,11 @@ Public Class frmTitleScreen
         frmAchievements.Show()
         Me.Hide()
     End Sub
+
+    Private Sub btnLeaderboard_Click(sender As Object, e As EventArgs) Handles btnLeaderboard.Click
+        frmLeaderboard.Show()
+        Me.Hide()
+    End Sub
 End Class
 Friend NotInheritable Class A
     Public Shared TilesFlagged As Integer = 0
@@ -38,7 +40,51 @@ Friend NotInheritable Class C
     Public Shared DifficultyHard As Integer = 0
     Public Shared DifficultyInsane As Integer = 0
 End Class
-
-Friend NotInheritable Class D
+Friend NotInheritable Class BB
+    Public Shared Constant2 As Integer = 1
+End Class
+Friend NotInheritable Class AC
+    Public Shared Achievements3 As Integer = 0
+End Class
+Friend NotInheritable Class BC
+    Public Shared Constant3 As Integer = 1
+End Class
+Friend NotInheritable Class AD
+    Public Shared Achievements4 As Integer = 0
+End Class
+Friend NotInheritable Class BD
+    Public Shared Constant4 As Integer = 1
+End Class
+Friend NotInheritable Class AE
+    Public Shared Achievements5 As Integer = 0
+End Class
+Friend NotInheritable Class BE
+    Public Shared Constant5 As Integer = 1
+End Class
+Friend NotInheritable Class AF
+    Public Shared Achievements6 As Integer = 0
+End Class
+Friend NotInheritable Class BF
+    Public Shared Constant6 As Integer = 1
+End Class
+Friend NotInheritable Class AG
+    Public Shared Achievements7 As Integer = 0
+End Class
+Friend NotInheritable Class BG
+    Public Shared Constant7 As Integer = 1
+End Class
+Friend NotInheritable Class AH
+    Public Shared Achievements8 As Integer = 0
+End Class
+Friend NotInheritable Class BH
+    Public Shared Constant8 As Integer = 1
+End Class
+Friend NotInheritable Class AI
+    Public Shared Achievements9 As Integer = 0
+End Class
+Friend NotInheritable Class BI
+    Public Shared Constant9 As Integer = 1
+End Class
+Friend NotInheritable Class DB
     Public Shared Difficulty As Integer = 0
 End Class
