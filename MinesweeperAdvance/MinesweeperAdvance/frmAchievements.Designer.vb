6d82f7cd8649e10cbbfdc9d9b69afe4b829a4eb1
Randil FERNANDO <fer0050@mhs.vic.edu.au>
2020-11-13 11:46:07 +1100

    Changed achievements layout and (possibly) fixed the way the achievements work

--- MinesweeperAdvance/MinesweeperAdvance/frmAchievements.Designer.vb
+++ MinesweeperAdvance/MinesweeperAdvance/frmAchievements.Designer.vb
@@ -27,16 +27,8 @@ Partial Class frmAchievements
         Me.lblAchievements1 = New System.Windows.Forms.Label()
         Me.lblAchievements2 = New System.Windows.Forms.Label()
         Me.lblAchievements3 = New System.Windows.Forms.Label()
-        Me.lblAchievements6 = New System.Windows.Forms.Label()
         Me.lblAchievements5 = New System.Windows.Forms.Label()
         Me.lblAchievements4 = New System.Windows.Forms.Label()
-        Me.lblAchievements9 = New System.Windows.Forms.Label()
-        Me.lblAchievements8 = New System.Windows.Forms.Label()
-        Me.lblAchievements7 = New System.Windows.Forms.Label()
-        Me.picAchievements9 = New System.Windows.Forms.PictureBox()
-        Me.picAchievements8 = New System.Windows.Forms.PictureBox()
-        Me.picAchievements7 = New System.Windows.Forms.PictureBox()
-        Me.picAchievements6 = New System.Windows.Forms.PictureBox()
         Me.picAchievements5 = New System.Windows.Forms.PictureBox()
         Me.picAchievements4 = New System.Windows.Forms.PictureBox()
         Me.picAchievements3 = New System.Windows.Forms.PictureBox()
@@ -44,10 +36,6 @@ Partial Class frmAchievements
         Me.picAchievements1 = New System.Windows.Forms.PictureBox()
         Me.btnBackAchievements = New System.Windows.Forms.Button()
         CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.picAchievements9, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.picAchievements8, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.picAchievements7, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.picAchievements6, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picAchievements5, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picAchievements4, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picAchievements3, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -62,150 +50,56 @@ Partial Class frmAchievements
         '
         'lblAchievements1
         '
-        Me.lblAchievements1.Location = New System.Drawing.Point(31, 78)
-        Me.lblAchievements1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblAchievements1.Location = New System.Drawing.Point(52, 155)
         Me.lblAchievements1.Name = "lblAchievements1"
-        Me.lblAchievements1.Size = New System.Drawing.Size(85, 19)
+        Me.lblAchievements1.Size = New System.Drawing.Size(128, 29)
         Me.lblAchievements1.TabIndex = 9
         Me.lblAchievements1.Text = "Label1"
         Me.lblAchievements1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'lblAchievements2
         '
-        Me.lblAchievements2.Location = New System.Drawing.Point(198, 78)
-        Me.lblAchievements2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblAchievements2.Location = New System.Drawing.Point(303, 155)
         Me.lblAchievements2.Name = "lblAchievements2"
-        Me.lblAchievements2.Size = New System.Drawing.Size(85, 19)
+        Me.lblAchievements2.Size = New System.Drawing.Size(128, 29)
         Me.lblAchievements2.TabIndex = 10
         Me.lblAchievements2.Text = "Label2"
         Me.lblAchievements2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'lblAchievements3
         '
-        Me.lblAchievements3.Location = New System.Drawing.Point(365, 78)
-        Me.lblAchievements3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblAchievements3.Location = New System.Drawing.Point(554, 155)
         Me.lblAchievements3.Name = "lblAchievements3"
-        Me.lblAchievements3.Size = New System.Drawing.Size(85, 19)
+        Me.lblAchievements3.Size = New System.Drawing.Size(128, 29)
         Me.lblAchievements3.TabIndex = 11
         Me.lblAchievements3.Text = "Label3"
         Me.lblAchievements3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'lblAchievements6
-        '
-        Me.lblAchievements6.Location = New System.Drawing.Point(365, 172)
-        Me.lblAchievements6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblAchievements6.Name = "lblAchievements6"
-        Me.lblAchievements6.Size = New System.Drawing.Size(85, 19)
-        Me.lblAchievements6.TabIndex = 14
-        Me.lblAchievements6.Text = "Label4"
-        Me.lblAchievements6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
         'lblAchievements5
         '
-        Me.lblAchievements5.Location = New System.Drawing.Point(198, 172)
-        Me.lblAchievements5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblAchievements5.Location = New System.Drawing.Point(431, 366)
         Me.lblAchievements5.Name = "lblAchievements5"
-        Me.lblAchievements5.Size = New System.Drawing.Size(85, 19)
+        Me.lblAchievements5.Size = New System.Drawing.Size(128, 29)
         Me.lblAchievements5.TabIndex = 13
         Me.lblAchievements5.Text = "Label5"
         Me.lblAchievements5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'lblAchievements4
         '
-        Me.lblAchievements4.Location = New System.Drawing.Point(31, 172)
-        Me.lblAchievements4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblAchievements4.Location = New System.Drawing.Point(173, 366)
         Me.lblAchievements4.Name = "lblAchievements4"
-        Me.lblAchievements4.Size = New System.Drawing.Size(85, 19)
+        Me.lblAchievements4.Size = New System.Drawing.Size(128, 29)
         Me.lblAchievements4.TabIndex = 12
         Me.lblAchievements4.Text = "Label6"
         Me.lblAchievements4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblAchievements9
-        '
-        Me.lblAchievements9.Location = New System.Drawing.Point(365, 265)
-        Me.lblAchievements9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblAchievements9.Name = "lblAchievements9"
-        Me.lblAchievements9.Size = New System.Drawing.Size(85, 19)
-        Me.lblAchievements9.TabIndex = 17
-        Me.lblAchievements9.Text = "Label7"
-        Me.lblAchievements9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblAchievements8
-        '
-        Me.lblAchievements8.Location = New System.Drawing.Point(198, 265)
-        Me.lblAchievements8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblAchievements8.Name = "lblAchievements8"
-        Me.lblAchievements8.Size = New System.Drawing.Size(85, 19)
-        Me.lblAchievements8.TabIndex = 16
-        Me.lblAchievements8.Text = "Label8"
-        Me.lblAchievements8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblAchievements7
-        '
-        Me.lblAchievements7.Location = New System.Drawing.Point(31, 265)
-        Me.lblAchievements7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblAchievements7.Name = "lblAchievements7"
-        Me.lblAchievements7.Size = New System.Drawing.Size(85, 19)
-        Me.lblAchievements7.TabIndex = 15
-        Me.lblAchievements7.Text = "Label9"
-        Me.lblAchievements7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'picAchievements9
-        '
-        Me.picAchievements9.Image = CType(resources.GetObject("picAchievements9.Image"), System.Drawing.Image)
-        Me.picAchievements9.InitialImage = CType(resources.GetObject("picAchievements9.InitialImage"), System.Drawing.Image)
-        Me.picAchievements9.Location = New System.Drawing.Point(365, 201)
-        Me.picAchievements9.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.picAchievements9.Name = "picAchievements9"
-        Me.picAchievements9.Size = New System.Drawing.Size(85, 62)
-        Me.picAchievements9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.picAchievements9.TabIndex = 8
-        Me.picAchievements9.TabStop = False
-        '
-        'picAchievements8
-        '
-        Me.picAchievements8.Image = CType(resources.GetObject("picAchievements8.Image"), System.Drawing.Image)
-        Me.picAchievements8.InitialImage = CType(resources.GetObject("picAchievements8.InitialImage"), System.Drawing.Image)
-        Me.picAchievements8.Location = New System.Drawing.Point(198, 201)
-        Me.picAchievements8.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.picAchievements8.Name = "picAchievements8"
-        Me.picAchievements8.Size = New System.Drawing.Size(85, 62)
-        Me.picAchievements8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.picAchievements8.TabIndex = 7
-        Me.picAchievements8.TabStop = False
-        '
-        'picAchievements7
-        '
-        Me.picAchievements7.Image = CType(resources.GetObject("picAchievements7.Image"), System.Drawing.Image)
-        Me.picAchievements7.InitialImage = CType(resources.GetObject("picAchievements7.InitialImage"), System.Drawing.Image)
-        Me.picAchievements7.Location = New System.Drawing.Point(31, 201)
-        Me.picAchievements7.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.picAchievements7.Name = "picAchievements7"
-        Me.picAchievements7.Size = New System.Drawing.Size(85, 62)
-        Me.picAchievements7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.picAchievements7.TabIndex = 6
-        Me.picAchievements7.TabStop = False
-        '
-        'picAchievements6
-        '
-        Me.picAchievements6.Image = CType(resources.GetObject("picAchievements6.Image"), System.Drawing.Image)
-        Me.picAchievements6.InitialImage = CType(resources.GetObject("picAchievements6.InitialImage"), System.Drawing.Image)
-        Me.picAchievements6.Location = New System.Drawing.Point(365, 109)
-        Me.picAchievements6.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.picAchievements6.Name = "picAchievements6"
-        Me.picAchievements6.Size = New System.Drawing.Size(85, 62)
-        Me.picAchievements6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.picAchievements6.TabIndex = 5
-        Me.picAchievements6.TabStop = False
         '
         'picAchievements5
         '
         Me.picAchievements5.Image = CType(resources.GetObject("picAchievements5.Image"), System.Drawing.Image)
         Me.picAchievements5.InitialImage = CType(resources.GetObject("picAchievements5.InitialImage"), System.Drawing.Image)
-        Me.picAchievements5.Location = New System.Drawing.Point(198, 109)
-        Me.picAchievements5.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.picAchievements5.Location = New System.Drawing.Point(431, 268)
         Me.picAchievements5.Name = "picAchievements5"
-        Me.picAchievements5.Size = New System.Drawing.Size(85, 62)
+        Me.picAchievements5.Size = New System.Drawing.Size(128, 95)
         Me.picAchievements5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picAchievements5.TabIndex = 4
         Me.picAchievements5.TabStop = False
@@ -214,10 +108,9 @@ Partial Class frmAchievements
         '
         Me.picAchievements4.Image = CType(resources.GetObject("picAchievements4.Image"), System.Drawing.Image)
         Me.picAchievements4.InitialImage = CType(resources.GetObject("picAchievements4.InitialImage"), System.Drawing.Image)
-        Me.picAchievements4.Location = New System.Drawing.Point(31, 109)
-        Me.picAchievements4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.picAchievements4.Location = New System.Drawing.Point(173, 268)
         Me.picAchievements4.Name = "picAchievements4"
-        Me.picAchievements4.Size = New System.Drawing.Size(85, 62)
+        Me.picAchievements4.Size = New System.Drawing.Size(128, 95)
         Me.picAchievements4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picAchievements4.TabIndex = 3
         Me.picAchievements4.TabStop = False
@@ -226,10 +119,9 @@ Partial Class frmAchievements
         '
         Me.picAchievements3.Image = CType(resources.GetObject("picAchievements3.Image"), System.Drawing.Image)
         Me.picAchievements3.InitialImage = CType(resources.GetObject("picAchievements3.InitialImage"), System.Drawing.Image)
-        Me.picAchievements3.Location = New System.Drawing.Point(365, 8)
-        Me.picAchievements3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.picAchievements3.Location = New System.Drawing.Point(554, 47)
         Me.picAchievements3.Name = "picAchievements3"
-        Me.picAchievements3.Size = New System.Drawing.Size(85, 62)
+        Me.picAchievements3.Size = New System.Drawing.Size(128, 95)
         Me.picAchievements3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picAchievements3.TabIndex = 2
         Me.picAchievements3.TabStop = False
@@ -238,10 +130,9 @@ Partial Class frmAchievements
         '
         Me.picAchievements2.Image = CType(resources.GetObject("picAchievements2.Image"), System.Drawing.Image)
         Me.picAchievements2.InitialImage = CType(resources.GetObject("picAchievements2.InitialImage"), System.Drawing.Image)
-        Me.picAchievements2.Location = New System.Drawing.Point(198, 8)
-        Me.picAchievements2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.picAchievements2.Location = New System.Drawing.Point(303, 47)
         Me.picAchievements2.Name = "picAchievements2"
-        Me.picAchievements2.Size = New System.Drawing.Size(85, 62)
+        Me.picAchievements2.Size = New System.Drawing.Size(128, 95)
         Me.picAchievements2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picAchievements2.TabIndex = 1
         Me.picAchievements2.TabStop = False
@@ -251,58 +142,43 @@ Partial Class frmAchievements
         Me.picAchievements1.Image = CType(resources.GetObject("picAchievements1.Image"), System.Drawing.Image)
         Me.picAchievements1.ImageLocation = ""
         Me.picAchievements1.InitialImage = CType(resources.GetObject("picAchievements1.InitialImage"), System.Drawing.Image)
-        Me.picAchievements1.Location = New System.Drawing.Point(31, 8)
-        Me.picAchievements1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.picAchievements1.Location = New System.Drawing.Point(52, 47)
         Me.picAchievements1.Name = "picAchievements1"
-        Me.picAchievements1.Size = New System.Drawing.Size(85, 62)
+        Me.picAchievements1.Size = New System.Drawing.Size(128, 95)
         Me.picAchievements1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picAchievements1.TabIndex = 0
         Me.picAchievements1.TabStop = False
         '
         'btnBackAchievements
         '
-        Me.btnBackAchievements.Location = New System.Drawing.Point(460, 246)
-        Me.btnBackAchievements.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnBackAchievements.Location = New System.Drawing.Point(690, 378)
         Me.btnBackAchievements.Name = "btnBackAchievements"
-        Me.btnBackAchievements.Size = New System.Drawing.Size(71, 38)
+        Me.btnBackAchievements.Size = New System.Drawing.Size(106, 58)
         Me.btnBackAchievements.TabIndex = 18
         Me.btnBackAchievements.Text = "Back"
         Me.btnBackAchievements.UseVisualStyleBackColor = True
         '
         'frmAchievements
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.AppWorkspace
-        Me.ClientSize = New System.Drawing.Size(533, 292)
+        Me.ClientSize = New System.Drawing.Size(800, 449)
         Me.Controls.Add(Me.btnBackAchievements)
-        Me.Controls.Add(Me.lblAchievements9)
-        Me.Controls.Add(Me.lblAchievements8)
-        Me.Controls.Add(Me.lblAchievements7)
-        Me.Controls.Add(Me.lblAchievements6)
         Me.Controls.Add(Me.lblAchievements5)
         Me.Controls.Add(Me.lblAchievements4)
         Me.Controls.Add(Me.lblAchievements3)
         Me.Controls.Add(Me.lblAchievements2)
         Me.Controls.Add(Me.lblAchievements1)
-        Me.Controls.Add(Me.picAchievements9)
-        Me.Controls.Add(Me.picAchievements8)
-        Me.Controls.Add(Me.picAchievements7)
-        Me.Controls.Add(Me.picAchievements6)
         Me.Controls.Add(Me.picAchievements5)
         Me.Controls.Add(Me.picAchievements4)
         Me.Controls.Add(Me.picAchievements3)
         Me.Controls.Add(Me.picAchievements2)
         Me.Controls.Add(Me.picAchievements1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "frmAchievements"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "frmAchievements"
         CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.picAchievements9, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.picAchievements8, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.picAchievements7, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.picAchievements6, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picAchievements5, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picAchievements4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picAchievements3, System.ComponentModel.ISupportInitialize).EndInit()
@@ -313,23 +189,15 @@ Partial Class frmAchievements
     End Sub
 
     Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
-    Friend WithEvents picAchievements6 As PictureBox
     Friend WithEvents picAchievements5 As PictureBox
     Friend WithEvents picAchievements4 As PictureBox
     Friend WithEvents picAchievements3 As PictureBox
     Friend WithEvents picAchievements2 As PictureBox
     Friend WithEvents picAchievements1 As PictureBox
-    Friend WithEvents lblAchievements9 As Label
-    Friend WithEvents lblAchievements8 As Label
-    Friend WithEvents lblAchievements7 As Label
-    Friend WithEvents lblAchievements6 As Label
     Friend WithEvents lblAchievements5 As Label
     Friend WithEvents lblAchievements4 As Label
     Friend WithEvents lblAchievements3 As Label
     Friend WithEvents lblAchievements2 As Label
     Friend WithEvents lblAchievements1 As Label
-    Friend WithEvents picAchievements9 As PictureBox
-    Friend WithEvents picAchievements8 As PictureBox
-    Friend WithEvents picAchievements7 As PictureBox
     Friend WithEvents btnBackAchievements As Button
 End Class
--- MinesweeperAdvance/MinesweeperAdvance/frmAchievements.vb
+++ MinesweeperAdvance/MinesweeperAdvance/frmAchievements.vb
@@ -1,32 +1,27 @@
 ﻿Public Class frmAchievements
     Dim TilesFlaggedLocal As Integer
+    Dim BarsFilledLocal As Integer
+    Dim DifficultyEasyLocal As Integer
+    Dim DifficultyMediumLocal As Integer
+    Dim DifficultyHardLocal As Integer
+    Dim DifficultyInsaneLocal As Integer
 
     Dim Ach1 As Integer = 0 + TilesFlaggedLocal
-    Dim Cons1 As Integer = 1
+    Dim Cons1 As Integer = 10
 
-    Dim Ach2 As Integer = 0
+    Dim Ach2 As Integer = 0 + BarsFilledLocal
     Dim Cons2 As Integer = 1
 
     Dim Ach3 As Integer = 0
     Dim Cons3 As Integer = 1
 
     Dim Ach4 As Integer = 0
-    Dim Cons4 As Integer = 1
+    Dim Cons4 As String = "Difficulty Easy"
 
     Dim Ach5 As Integer = 0
-    Dim Cons5 As Integer = 1
+    Dim Cons5 As Integer = 50000
 
-    Dim Ach6 As Integer = 0
-    Dim Cons6 As Integer = 1
 
-    Dim Ach7 As Integer = 0
-    Dim Cons7 As Integer = 1
-
-    Dim Ach8 As Integer = 0
-    Dim Cons8 As Integer = 1
-
-    Dim Ach9 As Integer = 0
-    Dim Cons9 As Integer = 1
 
 
     Private Sub btnBackAchievements_Click(sender As Object, e As EventArgs) Handles btnBackAchievements.Click
@@ -50,29 +45,53 @@
             Cons3 *= 10
         End If
         lblAchievements4.Text = Ach4 & " / " & Cons4
-        If (Ach4 >= Cons4) Then
-            Cons4 *= 10
+        If (Ach4 >= DifficultyEasyLocal) Then
+            Cons4 = "Difficulty Medium"
+            If (Ach4 >= DifficultyMediumLocal) Then
+                Cons4 = "Difficulty Hard"
+                If (Ach4 >= DifficultyHardLocal) Then
+                    Cons4 = "Difficulty Insane"
+                    If (Ach4 >= DifficultyInsaneLocal) Then
+                        Cons4 = "How"
+                    End If
+                End If
+            End If
         End If
         lblAchievements5.Text = Ach5 & " / " & Cons5
         If (Ach5 >= Cons5) Then
-            Cons5 *= 10
+            Cons5 = 100000
+            If (Ach5 >= Cons5) Then
+                Cons5 = 200000
+                If (Ach5 >= Cons5) Then
+                    Cons5 = 300000
+                End If
+            End If
         End If
-        lblAchievements6.Text = Ach6 & " / " & Cons6
-        If (Ach6 >= Cons6) Then
-            Cons6 *= 10
-        End If
-        lblAchievements7.Text = Ach7 & " / " & Cons7
-        If (Ach7 >= Cons7) Then
-            Cons7 *= 10
-        End If
-        lblAchievements8.Text = Ach8 & " / " & Cons8
-        If (Ach8 >= Cons8) Then
-            Cons8 *= 10
-        End If
-        lblAchievements9.Text = Ach9 & " / " & Cons9
-        If (Ach9 >= Cons9) Then
-            Cons9 *= 10
-        End If
+
+    End Sub
+
+    Private Sub lblAchievements1_Click(sender As Object, e As EventArgs) Handles lblAchievements1.Click
+
+    End Sub
+
+    Private Sub picAchievements1_Click(sender As Object, e As EventArgs) Handles picAchievements1.Click
+
+    End Sub
+
+    Private Sub lblAchievements2_Click(sender As Object, e As EventArgs) Handles lblAchievements2.Click
+
+    End Sub
+
+    Private Sub picAchievements2_Click(sender As Object, e As EventArgs) Handles picAchievements2.Click
+
+    End Sub
+
+    Private Sub lblAchievements3_Click(sender As Object, e As EventArgs) Handles lblAchievements3.Click
+
+    End Sub
+
+    Private Sub picAchievements3_Click(sender As Object, e As EventArgs) Handles picAchievements3.Click
+
     End Sub
 End Class
 
