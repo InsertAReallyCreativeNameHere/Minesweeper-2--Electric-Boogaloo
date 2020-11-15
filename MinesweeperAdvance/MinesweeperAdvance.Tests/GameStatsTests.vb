7057afc65caeb3269895b297d92d822980c21526
Andrew Eyhorn <andrew.eyhorn@gmail.com>
2020-11-15 17:41:31 +1100

    Added project for tests

---
+++ MinesweeperAdvance/MinesweeperAdvance.Tests/GameStatsTests.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class GameStatsTests
+
+    <TestMethod()> Public Sub CheckTotals()
+        'Arrange
+        Dim stats = New Statistics
+        stats.Easy.HighScore = 5
+        stats.Easy.GamesPlayed = 3
+
+        stats.Medium.HighScore = 2
+        stats.Medium.GamesPlayed = 50
+
+        'Act
+        Dim overallStats = stats.GetOverallStats()
+
+        'Assert
+        Assert.AreEqual(5, overallStats.HighScore)
+        Assert.AreEqual(53, overallStats.GamesPlayed)
+    End Sub
+
+End Class
---
+++ MinesweeperAdvance/MinesweeperAdvance.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("MinesweeperAdvance.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MinesweeperAdvance.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("eb675f76-5998-418f-9064-91899524d63f")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
