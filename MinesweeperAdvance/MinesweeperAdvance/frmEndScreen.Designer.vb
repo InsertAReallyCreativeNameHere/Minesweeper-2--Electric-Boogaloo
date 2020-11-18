ae6fa3cda469b606b346b3c5355dc1bf2c0de790
Randil FERNANDO <fer0050@mhs.vic.edu.au>
2020-11-18 13:01:49 +1100

    finalised design

--- MinesweeperAdvance/MinesweeperAdvance/frmEndScreen.Designer.vb
+++ MinesweeperAdvance/MinesweeperAdvance/frmEndScreen.Designer.vb
@@ -24,15 +24,18 @@ Partial Class frmEndScreen
     Private Sub InitializeComponent()
         Me.lblEndTitle = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnRetry = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblEndTitle
         '
         Me.lblEndTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblEndTitle.Location = New System.Drawing.Point(8, 10)
+        Me.lblEndTitle.Location = New System.Drawing.Point(12, 15)
+        Me.lblEndTitle.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblEndTitle.Name = "lblEndTitle"
-        Me.lblEndTitle.Size = New System.Drawing.Size(367, 52)
+        Me.lblEndTitle.Size = New System.Drawing.Size(550, 80)
         Me.lblEndTitle.TabIndex = 0
         Me.lblEndTitle.Text = "Label1"
         Me.lblEndTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -40,31 +43,54 @@ Partial Class frmEndScreen
         'Label1
         '
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(8, 62)
+        Me.Label1.Location = New System.Drawing.Point(45, 164)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(367, 52)
+        Me.Label1.Size = New System.Drawing.Size(207, 80)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Score"
+        Me.Label1.Text = "Score:"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnRetry
+        '
+        Me.btnRetry.Location = New System.Drawing.Point(55, 322)
+        Me.btnRetry.Name = "btnRetry"
+        Me.btnRetry.Size = New System.Drawing.Size(227, 129)
+        Me.btnRetry.TabIndex = 3
+        Me.btnRetry.Text = "Retry"
+        Me.btnRetry.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(288, 322)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(227, 129)
+        Me.btnQuit.TabIndex = 4
+        Me.btnQuit.Text = "Back"
+        Me.btnQuit.UseVisualStyleBackColor = True
         '
         'Label2
         '
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(11, 114)
+        Me.Label2.Location = New System.Drawing.Point(235, 164)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(367, 52)
+        Me.Label2.Size = New System.Drawing.Size(313, 80)
         Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Score"
+        Me.Label2.Text = "000000"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'frmEndScreen
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(384, 361)
+        Me.ClientSize = New System.Drawing.Size(576, 555)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnRetry)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.lblEndTitle)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "frmEndScreen"
         Me.Text = "frmEndScreen"
         Me.ResumeLayout(False)
@@ -73,5 +99,7 @@ Partial Class frmEndScreen
 
     Friend WithEvents lblEndTitle As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents btnRetry As Button
+    Friend WithEvents btnQuit As Button
     Friend WithEvents Label2 As Label
 End Class
--- MinesweeperAdvance/MinesweeperAdvance/frmEndScreen.vb
+++ MinesweeperAdvance/MinesweeperAdvance/frmEndScreen.vb
@@ -1,3 +1,11 @@
 ﻿Public Class frmEndScreen
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Me.Hide()
+        frmTitleScreen.Show()
+    End Sub
 
+    Private Sub btnRetry_Click(sender As Object, e As EventArgs) Handles btnRetry.Click
+        Me.Hide()
+        frmGameSelection.Show()
+    End Sub
 End Class
